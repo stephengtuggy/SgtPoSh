dbd2507a16968869a021bbe7bddbb9388a4d7733
Stephen G Tuggy <sgt@stephengtuggy.com>
2016-06-12 11:49:48 -0700

    Initial commit of PowerShell PackageManagement setup scripts

---
+++ PackageManagement/Step3-Set-PackageSource-All3-Trusted.ps1
@@ -0,0 +1,36 @@
+# Step3-Set-PackageSource-All3-Trusted.ps1
+#
+# Step 3 of my PowerShell PackageManagement setup process
+#
+# Created  2016-06-12 by Stephen Tuggy
+# Modified 2016-06-12 by Stephen Tuggy
+# Version 0.1.0
+# Runs with Windows PowerShell 5.0
+# 
+# The MIT License (MIT)
+# 
+# Copyright (c) 2016 Stephen G Tuggy
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+Set-PackageSource -Name chocolatey -Location http://chocolatey.org/api/v2/ -NewLocation https://chocolatey.org/api/v2/ -ProviderName chocolatey -Trusted
+Set-PackageSource -Name PSGallery -Location https://www.powershellgallery.com/api/v2/ -ProviderName PowerShellGet -Trusted
+Set-PackageSource -Name NuGet.Org -Location https://www.nuget.org/api/v2 -ProviderName NuGet -Trusted
+
+Get-PackageSource
---
+++ PackageManagement/Step5-Install-DesiredPackages.ps1
@@ -0,0 +1,39 @@
+# Step5-Install-DesiredPackages.ps1
+#
+# Step 5 of my PowerShell PackageManagement setup process
+#
+# Created  2016-06-12 by Stephen Tuggy
+# Modified 2016-06-12 by Stephen Tuggy
+# Version 0.1.0
+# Runs with Windows PowerShell 5.0
+# 
+# The MIT License (MIT)
+# 
+# Copyright (c) 2016 Stephen G Tuggy
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+Find-Package -Name "7zip"                   -Source chocolatey | Install-Package
+Find-Package -Name firefox                  -Source chocolatey | Install-Package
+Find-Package -Name adblockplus-firefox      -Source chocolatey | Install-Package
+Find-Package -Name GoogleChrome             -Source chocolatey | Install-Package
+Find-Package -Name adblockpluschrome        -Source chocolatey | Install-Package
+Find-Package -Name opera                    -Source chocolatey | Install-Package
+Find-Package -Name adblockplusopera         -Source chocolatey | Install-Package
+Find-Package -Name git                      -Source chocolatey | Install-Package
