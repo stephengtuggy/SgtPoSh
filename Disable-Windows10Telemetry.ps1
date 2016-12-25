416861f64e8805a891cb047096dc4cceac65e0a0
stephengtuggy <sgt@stephengtuggy.com>
2016-12-24 16:37:20 -0800

    Add some info from majorgeeks.net

--- Disable-Windows10Telemetry.ps1
+++ Disable-Windows10Telemetry.ps1
@@ -4,12 +4,13 @@
 # components that are built into Windows 10.
 # 
 # Based in part on 
-# http://winaero.com/blog/how-to-disable-telemetry-and-data-collection-in-windows-10/
-# , especially the comments by user "Wade".
+#   * http://winaero.com/blog/how-to-disable-telemetry-and-data-collection-in-windows-10/ , especially the comments by user "Wade".
+#   * http://forums.majorgeeks.com/index.php?threads/services-to-disable-or-not-to.304471/
+#   * https://hideu.wordpress.com/2015/08/13/how-to-kill-windows-10-privacy-spying-forever/ (Yes, this guy may be excessively paranoid, but I found this particular item useful)
 # 
 # Created  2016-08-10
 # Modified 2016-12-24
-# Version 0.2.0
+# Version 0.2.2
 # Runs with Windows PowerShell
 # 
 # The MIT License (MIT)
@@ -38,29 +39,45 @@ trap [System.Exception] {
     Write-Warning "Trapped exception: $_"
 }
 
-$badServices = @(
+$servicesToDisable = @(
     'DiagTrack',                                    # Connected User Experiences and Telemetry
     'diagnosticshub.standardcollector.service',     # Microsoft (R) Diagnostics Hub Standard Collector Service
     'dmwappushservice',                             # (Based on what I've read on the Internet, this is a major part of the Windows 10 spyware infrastructure)
     'CDPSvc',                                       # Connected Devices Platform Service
-    'AJRouter',                                     # AllJoyn Router Service
-    #'ALG',                                         # Application Layer Gateway -- This apparently IS needed
     'PeerDistSvc',                                  # BranchCache
     'WerSvc',                                       # Windows Error Reporting Service
     'DcpSvc',                                       # DataCollectionPublishingService
-    'DoSvc',                                        # Delivery Optimization
     'DeviceAssociationService',                     # Device Association Service
-    'DmEnrollmentSvc',                              # Device Management Enrollment Service
-    'DevQueryBroker',                               # DevQuery Background Discovery Broker
     'DPS',                                          # Diagnostic Policy Service
     'lfsvc',                                        # Geolocation Service
     'MapsBroker',                                   # Downloaded Maps Manager
+    'BthHFSrv',                                     # Bluetooth Handsfree Service
+    'bthserv',                                      # Bluetooth Support Service -- Multiple sources say to disable this
+    'DsSvc',                                        # Data Sharing Service
     ''
 )
 
-foreach ($svc in $badServices) {
-    Get-Service $svc | Stop-Service -Force
-    Get-Service $svc | Set-Service -StartupType Disabled
+$servicesToSetToManual = @(
+    'ALG',                                          # Application Layer Gateway -- This apparently IS needed
+    'AJRouter',                                     # AllJoyn Router Service
+    'BITS',                                         # Background Intelligent Transfer Service
+    'DmEnrollmentSvc',                              # Device Management Enrollment Service
+    'DevQueryBroker',                               # DevQuery Background Discovery Broker
+    ''
+)
+
+$servicesToLeaveAlone = @(
+    'DoSvc',                                        # Delivery Optimization -- Apparently this is needed after all
+)
+
+foreach ($svc in $servicesToDisable) {
+    Get-Service $svc    | Stop-Service -Force
+    Get-Service $svc    | Set-Service -StartupType Disabled
 }
 
-reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection\ /v AllowTelemetry /t REG_DWORD /d 0 /f
+foreach ($svc2 in $servicesToSetToManual) {
+    Get-Service $svc2   | Set-Service -StartupType Manual
+}
+
+# reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection\ /v AllowTelemetry /t REG_DWORD /d 0 /f
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
